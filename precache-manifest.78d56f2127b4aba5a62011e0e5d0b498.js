06e4fb84476db3d204a687ab0b4bbc22c7eb25f3
BitFlippa27 <32537389+BitFlippa27@users.noreply.github.com>
2022-07-12 16:10:13 -0400

    Updates

---
+++ precache-manifest.78d56f2127b4aba5a62011e0e5d0b498.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c71450333f5170d9ad5e",
+    "url": "/roboamigos/static/css/main.dc9649b5.chunk.css"
+  },
+  {
+    "revision": "c71450333f5170d9ad5e",
+    "url": "/roboamigos/static/js/main.c7145033.chunk.js"
+  },
+  {
+    "revision": "d2b2cd7eb9ab7f1c5600",
+    "url": "/roboamigos/static/css/1.2eae6c89.chunk.css"
+  },
+  {
+    "revision": "d2b2cd7eb9ab7f1c5600",
+    "url": "/roboamigos/static/js/1.d2b2cd7e.chunk.js"
+  },
+  {
+    "revision": "30aa26c9a9d968960ab7",
+    "url": "/roboamigos/static/js/runtime~main.30aa26c9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboamigos/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ff432c85d35f0ed765b5d55714a59edd",
+    "url": "/roboamigos/index.html"
+  }
+];
